15f69bc05f85c987d22cc3785863e63b4fb56d44
Miguel Galea Lopez <250110028@itsoeh.edu.mx>
2026-07-02 15:04:53 -0600

    funcion 15 contar_clases

---
+++ src/asistencias.py
@@ -0,0 +1,38 @@
+"""
+Módulo de asistencias.
+
+Representación:
+1 = asistió
+0 = faltó
+"""
+
+
+def contar_clases(asistencias_alumno):
+
+    return len(asistencias_alumno)
+
+a=[1,1,0,1]
+print(contar_clases(a))
+
+def contar_asistencias(asistencias_alumno):
+    pass
+
+
+def contar_faltas(asistencias_alumno):
+    pass
+
+
+def porcentaje_asistencia(asistencias_alumno):
+    pass
+
+
+def esta_en_riesgo(asistencias_alumno):
+    pass
+
+
+def alumnos_en_riesgo(asistencias):
+    pass
+
+
+def promedio_asistencia_grupo(asistencias):
+    pass
