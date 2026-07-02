40d16a7f706b0f0c8bf866fd0645c3f36e9a06e7
José Antonio Martínez Aguilar <240110116@itsoeh.edu.mx>
2026-07-02 14:37:59 -0600

    Implementa función obtener_opciones

--- src/encuestas.py
+++ src/encuestas.py
@@ -10,7 +10,13 @@ def contar_respuestas(respuestas):
 
 
 def obtener_opciones(respuestas):
-    pass
+    opciones = []
+
+    for respuesta in respuestas:
+        if respuesta not in opciones:
+            opciones.append(respuesta)
+
+    return opciones
 
 
 def frecuencia_respuestas(respuestas):
