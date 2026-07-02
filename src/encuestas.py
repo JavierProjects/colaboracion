2e4a4405f0c1c9b6dfbad28d187e4782af5f5a53
José Antonio Martínez Aguilar <240110116@itsoeh.edu.mx>
2026-07-02 14:18:37 -0600

    Implementa función asignada contar_respuestas(respuestas)

--- src/encuestas.py
+++ src/encuestas.py
@@ -10,62 +10,20 @@ def contar_respuestas(respuestas):
 
 
 def obtener_opciones(respuestas):
-    opciones = []
+    pass
 
-    for respuesta in respuestas:
-        if respuesta not in opciones:
-            opciones.append(respuesta)
-
-    return opciones
 
 def frecuencia_respuestas(respuestas):
-    frecuencias = {}
-
-    for respuesta in respuestas:
-        if respuesta in frecuencias:
-            frecuencias[respuesta] += 1
-        else:
-            frecuencias[respuesta] = 1
-
-    return frecuencias
+    pass
 
 
 def respuesta_mas_comun(respuestas):
-    if len(respuestas) == 0:
-        return None
-
-    frecuencias = frecuencia_respuestas(respuestas)
-
-    mayor = 0
-    comun = None
-
-    for respuesta in frecuencias:
-        if frecuencias[respuesta] > mayor:
-            mayor = frecuencias[respuesta]
-            comun = respuesta
-
-    return comun
+    pass
 
 
 def porcentaje_respuesta(respuestas, opcion):
-    if len(respuestas) == 0:
-        return 0.0
-
-    contador = 0
-
-    for respuesta in respuestas:
-        if respuesta == opcion:
-            contador += 1
-
-    return (contador / len(respuestas)) * 100
+    pass
 
 
 def resumen_encuesta(respuestas):
-    resumen = {
-        "total": contar_respuestas(respuestas),
-        "opciones": obtener_opciones(respuestas),
-        "frecuencias": frecuencia_respuestas(respuestas),
-        "mas_comun": respuesta_mas_comun(respuestas)
-    }
-
-    return resumen
+    pass
