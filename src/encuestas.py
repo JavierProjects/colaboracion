b64bb3fa9080e882eca98f00031644a8fd0c1d77
José Antonio Martínez Aguilar <240110116@itsoeh.edu.mx>
2026-07-02 14:46:06 -0600

    Implementa función resumen_encuesta

--- src/encuestas.py
+++ src/encuestas.py
@@ -63,4 +63,11 @@ def porcentaje_respuesta(respuestas, opcion):
 
 
 def resumen_encuesta(respuestas):
-    pass
+    resumen = {
+        "total": contar_respuestas(respuestas),
+        "opciones": obtener_opciones(respuestas),
+        "frecuencias": frecuencia_respuestas(respuestas),
+        "mas_comun": respuesta_mas_comun(respuestas)
+    }
+
+    return resumen
