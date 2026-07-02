fddd9aa6644a2b8da45da16dec2a02c02b96b74c
Jonathan Jovany Monjaraz Perez <250110964@itsoeh.edu.mx>
2026-07-02 13:57:41 -0600

    Agrega función obtener_opciones

--- src/encuestas.py
+++ src/encuestas.py
@@ -5,3 +5,12 @@ def contar_respuestas(respuestas):
         total = total + 1
 
     return total
+
+def obtener_opciones(respuestas):
+    opciones = []
+
+    for respuesta in respuestas:
+        if respuesta not in opciones:
+            opciones.append(respuesta)
+
+    return opciones
