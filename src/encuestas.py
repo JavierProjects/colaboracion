5b178fd989abde2d297deef18c9acb305f1d8680
Jonathan Jovany Monjaraz Perez <250110964@itsoeh.edu.mx>
2026-07-07 13:51:21 -0600

    Implementa función frecuencia_respuestas

--- src/encuestas.py
+++ src/encuestas.py
@@ -24,7 +24,17 @@ def obtener_opciones(respuestas):
 
 
 def frecuencia_respuestas(respuestas):
-    pass
+    frecuencias = {}
+
+    for respuesta in respuestas:
+        if respuesta in frecuencias:
+            frecuencias[respuesta] = frecuencias[respuesta] + 1
+        else:
+            frecuencias[respuesta] = 1
+
+    return frecuencias
+
+
 
 
 def respuesta_mas_comun(respuestas):
