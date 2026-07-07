7da9ed42e6554db9b7e44ccb02af5699ecba6745
Oscar Jesús Pérez Ramírez <250110173@itsoeh.edu.mx>
2026-07-07 13:57:50 -0600

    Implementa función asignada (respuestas_mas_comun)

--- src/encuestas.py
+++ src/encuestas.py
@@ -20,6 +20,13 @@ def obtener_opciones(respuestas):
 
 
 def frecuencia_respuestas(respuestas):
+    frecuencias = {}
+    for respuesta in respuestas:
+        if respuesta in frecuencias:
+            frecuencias[respuesta] += 1
+        else:
+            frecuencias[respuesta] = 1
+    return frecuencias
     pass
 
 
