dc57531d8cfb6e54d80cfac7440d4fa078006e35
Sinai Mejia Hernandez <250110900@itsoeh.edu.mx>
2026-07-07 14:23:53 -0600

    funcion implementada en modulo1

--- src/calificaciones.py
+++ src/calificaciones.py
@@ -1,62 +1,77 @@
 """
-Módulo de calificaciones.
+Módulo de asistencias.
 
-Implementa estas funciones usando Python puro: listas, ciclos,
-condicionales, diccionarios y funciones.
+Representación:
+1 = asistió
+0 = faltó
 """
 
 
+def contar_clases(asistencias_alumno):
+    pass
+    
 def contar_calificaciones(calificaciones):
+    contador = 0
+    for calificacion in calificaciones:
+        contador += 1
+    return contador
+
+def contar_asistencias(asistencias_alumno):
     pass
-
-
 def sumar_calificaciones(calificaciones):
+    suma = 0
+
+    for calificacion in calificaciones:
+        suma += calificacion
+
+    return suma
+
+def contar_faltas(asistencias_alumno):
     pass
-
-
 def calificacion_maxima(calificaciones):
+    if calificaciones == []:
+        return None
+    maxima = calificaciones[0]
+    for calificacion in calificaciones:
+        if calificacion > maxima:
+            maxima = calificacion
+    return maxima
+
+def porcentaje_asistencia(asistencias_alumno):
     pass
-
-
 def calificacion_minima(calificaciones):
+    if calificaciones ==[]:
+        return None 
+    minima = calificacion[0]
+    for calificacion in calificaciones:
+        if calificaion < minima:
+            minima = calificacion
+    return minima 
+
+def esta_en_riesgo(asistencias_alumno):
     pass
-
-
 def contar_aprobados(calificaciones):
+    if calificaciones <=70:
+        return "Mayor que 70"
+    else:
+        return "Menor que 70"
+
+def alumnos_en_riesgo(asistencias):
     pass
-
-
 def contar_reprobados(calificaciones):
+    if calificaciones <70:
+        return "Aprobado"
+    else:
+        return "Reprobado"
+
+def promedio_asistencia_grupo(asistencias):
     pass
-
-
 def clasificar_calificacion(calificacion):
-    pass
-
-
-def promedio(calificaciones):
-    pass
-
-
-def porcentaje_aprobados(calificaciones):
-    pass
-
-
-def porcentaje_reprobados(calificaciones):
-    pass
-
-
-def frecuencia_calificaciones(calificaciones):
-    pass
-
-
-def moda(calificaciones):
-    pass
-
-
-def mediana(calificaciones):
-    pass
-
-
-def resumen_calificaciones(calificaciones):
-    pass
+    if calificacion >= 90:
+        return "Excelente"
+    elif calificacion >= 80:
+        return "Bueno"
+    elif calificacion >= 70:
+        return "Regular"
+    else:
+        return "Reprobado"
