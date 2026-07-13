7365ce0c371d69cb0fa3291ebeeeab1252ec82ce
Pérez_Mendoza_Christian <250110019@itsoeh.edu.mx>
2026-07-12 20:37:12 -0600

    Terminado el reporte

--- src/reportes.py
+++ src/reportes.py
@@ -3,7 +3,15 @@ Módulo de reportes.
 
 Aquí se integran resultados de calificaciones, asistencias y encuestas.
 """
-
+from src.calificaciones import promedio, porcentaje_aprobados
+from src.asistencias import promedio_asistencia_grupo
+from src.encuestas import respuesta_mas_comun
 
 def reporte_general(calificaciones, asistencias, respuestas):
-    pass
+    reporte = {
+        "promedio_calificaciones": promedio(calificaciones),
+        "porcentaje_aprobados": porcentaje_aprobados(calificaciones),
+        "promedio_asistencia": promedio_asistencia_grupo(asistencias),
+        "respuesta_mas_comun": respuesta_mas_comun(respuestas)
+    }
+    return reporte
